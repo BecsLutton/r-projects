7362d3442f3527c01e1ee3657993cebbde46b5e9
BecsLutton <rlutton01@gmail.com>
2023-04-17 13:23:31 +0100

    commenting

--- nisra-stats/nicei/NICEI.R
+++ nisra-stats/nicei/NICEI.R
@@ -1,3 +1,4 @@
+# installing relevant libraries
 library(readxl)
 library(janitor)
 library(ggplot2)
@@ -7,38 +8,52 @@ library(tidyverse)
 
 
 #==========cleaning===============
-df<- read_excel("~/Documents/IDES/r/r-projects-main/nisra-stats/nicei/NICEI-Tables-Q4-2022.xlsx",sheet = "Table 1")
+df <- read_excel("NICEI-Tables-Q4-2022.xlsx", sheet = "Table 1", skip = 1) #read in excel data from local directory
 
+# dropping unused columns and merging the Year and Quarter columns
 df1 <- df %>% 
-  row_to_names(row_number = 1) %>% 
-  select(c(0:5))
-
-df1$date <- paste(df1$Year, df1$Quarter, sep="\nQ")
-df1<-df1[, c(6, 1, 2, 3, 4, 5)]
-df2<-df1[,-c(1:3)]
-df2 <- as.data.frame(apply(df2, 2, as.numeric))
-df2$date<-(df1$date)
-df2<-df2 %>% relocate(date)
-sapply(df2, class)
+  select(c(0:5)) %>%
+  mutate(date = paste(Year, Quarter, sep="\nQ"), .before = 1, .keep = "unused")
 
 
 #============data plotting===============
-ggplot(df2, aes(date, NICEI, group=1)) + geom_line(color="black")
+ggplot(df1, aes(date, NICEI, group=1)) + geom_line(color="black") #testing NICEI data
 
-df3<-melt(df2, id.vars="date")
-ggnicei<-ggplot() + geom_line(data = df3, aes(x=date, y=value, group = variable, color = variable))
+# restructuring the data from the 4 columns to 3 that contain the date, variable (previous column headers) and their values
+# useful for colouring plots based on category as is needed in this case
+df2<-melt(df1, id.vars="date") 
 
+#plot of data
+ggnicei<-ggplot() + 
+  geom_line(data = df2, 
+            aes(x=date, y=value, 
+                group = variable, 
+                color = variable)
+            )
+ggnicei #sanity check
 
-
+#Create function to return a function as the breaks argument in order to return appropriate x-axis spacing
 every_nth = function(n) {
-  return(function(x) {x[c(TRUE, rep(FALSE, n - 1))]})
+  return(
+    function(x){
+      x[c(TRUE, rep(FALSE, n - 1))]
+      }
+    )
 }
 
+# create the final plot
 final_plot<-ggnicei + 
   scale_color_manual(values= c("black", "blue", "#cccc00"), 
-                     labels=c("NICEI", "Private Sector Component Index", "Public Sector Component Index")) +
+                     labels=c("NICEI", 
+                              "Private Sector Component Index", 
+                              "Public Sector Component Index")) +
   geom_hline(yintercept=100, linetype = 'dashed') + 
-  annotate("text", x="2014\nQ1", y= 99, label = "Baseline 2019 = 100", size =3, fontface = "bold") + 
+  annotate("text", 
+           x="2014\nQ1", 
+           y= 99, 
+           label = "Baseline 2019 = 100", 
+           size =3, 
+           fontface = "bold") + 
   theme(line = element_blank(),
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(),
@@ -52,14 +67,40 @@ final_plot<-ggnicei +
         plot.margin = unit(c(2,2,2,2), "cm")) +
   scale_x_discrete(breaks = every_nth(n = 8)) + 
   scale_y_continuous(breaks = seq(75, 110, by=5), limits=c(75,110))+
-  annotate(geom = "point", x = "2022\nQ4", y = 105.7, colour = "red", size = 2) +
-  annotate(geom = "label", x = "2021\nQ4", y = 108, label = "105.7", hjust = "left", fill="blue", color = "white") +
-  annotate("segment", x = "2022\nQ1", xend = "2022\nQ4", y = 107.8, yend = 105.7, linewidth = 0.2) +
-  labs(x = "", y="", title = "NICEI Trend to Q4 2022") 
+  annotate(geom = "point", 
+           x = "2022\nQ4", 
+           y = 105.7, 
+           colour = "red", 
+           size = 2) +
+  annotate(geom = "label", 
+           x = "2021\nQ4", 
+           y = 108, 
+           label = "105.7", 
+           hjust = "left", 
+           fill="blue", 
+           color = "white") +
+  annotate("segment", 
+           x = "2022\nQ1", 
+           xend = "2022\nQ4", 
+           y = 107.8, yend = 105.7,
+           linewidth = 0.2) +
+  labs(x = "", 
+       y="", 
+       title = "NICEI Trend to Q4 2022") 
+
+final_plot
 
 #The plot in the original pdf seems to have a thicker NICEI line; if this is desired, all that is required is to overlay the
 #final_plot with 
 
-df4<-filter(df3, variable == "NICEI")
-final_plot + geom_line(data=df4, aes(x=date, y=value, group = 1), linewidth = 1)
+df4<-filter(df2, variable == "NICEI")
+final_plot2<- final_plot + geom_line(data=df4, aes(x=date, y=value, group = 1), linewidth = 1)
+final_plot2
 
+# Instead of exporting manually, scripted export of plot image can be done
+# Note the decrease in text readability using this method. Text sizes need increased if using this method
+# Manual export if this plot is in the local directory named, "NICEI_r_plot.png"
+
+png("NICEI-plot.png", width = 950, height = 750)
+final_plot2
+dev.off()
--- nisra-stats/nicei/cont_Q_change.R
+++ nisra-stats/nicei/cont_Q_change.R
@@ -1,54 +1,121 @@
+# installing relevant libraries
 library(readxl)
 library(tidyverse)
 library(formattable)
 library(ggplot2)
 
-NICEI_Tables_Q4_2022 <- read_excel("NICEI-Tables-Q4-2022.xlsx", 
+#---------------- Data prep--------------------
+# read in relevant data range from Excel table in local directory
+df <- read_excel("NICEI-Tables-Q4-2022.xlsx", 
                                    sheet = "Table 6", 
-                                   range = "A2:D8")
-df<- NICEI_Tables_Q4_2022
-df1<-df[, 1:2, drop=FALSE] #If the drop argument is provided with the value FALSE the columns are not converted to vector objects.
-df1[5,1]<-""
+                                   range = "A2:B8")
 
-formattable(df1)
+# Replace row 5 column 1 NA with empty space.
+# The plot in the publication to be replicated has an evident gap between NICEI bar and rest
+df[5,1]<-""
 
-ggplot(df1, aes(Sector, `Quarterly contribution (pps)*`))  + geom_col()  +coord_flip()
+formattable(df)     #table check
 
-df2<-map_df(df1, rev)
-df2$category<- c("A", "B", "B", "B", "B", "C")
-formattable(df2)
+#plot histogram to check layout and comparison to original
+ggplot(df,                                            # data
+       aes(Sector,                                    # X values
+           `Quarterly contribution (pps)*`))  +       # Y values
+  geom_col() +                                        # Plot type
+  coord_flip()                                        # Horizontal
 
-hbplt2<-ggplot(df2, aes(Sector, `Quarterly contribution (pps)*`, fill = category)) + geom_col() + coord_flip()
-hbplt2
+#--------------- Plot prep---------------------
+df1<-map_df(df, rev)      # reverse the order of the columns in df
+df1$category<- c("A", "B", "B", "B", "B", "C")      # creating a new column to assist with colour scheme in original
+formattable(df1)    # table check
 
-df3<-df2
-df3$order<-c(1:nrow(df3))
-formattable(df3)
+# Creating similar plot as above but with 3 colours
+hbplt<-ggplot(df1, 
+              aes(Sector, 
+                  `Quarterly contribution (pps)*`, 
+                  fill = category)) + 
+  geom_col() + 
+  coord_flip()
 
-final_plot<-df3 %>% 
-  ggplot(df3, mapping=aes(fct_reorder(Sector, desc(order)), `Quarterly contribution (pps)*`, fill = category)) + 
-  geom_col(width = 0.25) +
-  theme_classic() +
-  geom_text(aes(label = `Quarterly contribution (pps)*`), hjust = -1.25)
+hbplt
+#note the plot doesn't seem to plot bars in order of table. 
 
-final_plot <- df3 %>% 
-  ggplot(df3, mapping=aes(fct_reorder(Sector, desc(order)), `Quarterly contribution (pps)*`, fill = category)) + 
-  geom_col(width = 0.25) +
-  theme_classic() +
-  geom_text(aes(label=ifelse( `Quarterly contribution (pps)*` < 0, `Quarterly contribution (pps)*`, "")), hjust = 1.1) +
-  geom_text(aes(label=ifelse( `Quarterly contribution (pps)*` > 0, `Quarterly contribution (pps)*`, "")), hjust = -0.2) +
-  scale_fill_manual(values= c("#000066", "blue", "#cccc00")) +
-  theme(aspect.ratio = 1/2,
-        plot.margin = unit(c(2,2,2,2), "cm"),
-        panel.background = element_blank(),
-        panel.grid.major.x = element_line(color = "grey"),
-        plot.title = element_text(hjust = 0.5, face="bold"),
-        axis.text = element_text(size=10, face = "bold"),
-        axis.line = element_line(colour = "grey"),
-        axis.ticks = element_blank(),
-        legend.position="none") +
-  coord_flip()+
-  scale_y_continuous(limits=c(-0.5,1.5), expand = c(0, 0)) +
-  labs(x = "", y="", title = "Contributions of component indices to quarterly change in \nNICEI** (pps)") 
+df2<-df1        # creating new data frame based on previous
 
-final_plot
+df2$order<-c(1:nrow(df2))     # adding row attributing an order for the plot     
+
+
+#----------------Plotting-------------------
+
+final_plot <- ggplot(df2, 
+                     aes(fct_reorder(Sector, desc(order)),   # attributes a sequence of plotting Sector based on 'order' 
+                         `Quarterly contribution (pps)*`,
+                         fill = category)) + 
+  geom_col(width = 0.25) +          # adjusts the column widths so they are thinner
+  theme_classic() +           # blank background as per original image
+  geom_text(aes(label =         # adding data labels to the plot
+                  ifelse(`Quarterly contribution (pps)*` < 0, `Quarterly contribution (pps)*`, "")),   # if value is negative 
+            hjust = 1.1) +     # position label on left
+  geom_text(aes(label =
+                  ifelse( `Quarterly contribution (pps)*` > 0, `Quarterly contribution (pps)*`, "")),  # if value is negative 
+            hjust = -0.2) +    # position label on left
+  scale_fill_manual(values= c("#000066",         # colour first category 
+                              "blue",            # colour second category 
+                              "#cccc00")) +      # colour third category 
+  theme(aspect.ratio = 1/2,                                         # change aspect ratio of plot
+        plot.margin = unit(c(2,2,2,2), "cm"),                       # pad area around plot for image capture
+        panel.grid.major.x = element_line(color = "grey"),          # colour major gridlines grey
+        plot.title = element_text(hjust = 0.5,                      # centre the plot title and bolden
+                                  face="bold"),
+        axis.text = element_text(size=10, face =                    # make axis text bold and size 10
+                                   "bold"),
+        axis.line = element_line(colour = "grey"),                  # colour axis lines grey 
+        axis.ticks = element_blank(),                               # remove axis ticks
+        legend.position="none") +                                   # no legend
+  coord_flip()+                                  # flip plot to horizontal         
+  scale_y_continuous(limits=c(-0.5,1.5),         # alter y axis to start at -0.5 and end at 1.5
+                     expand = c(0, 0)) +         # y axis limits to start at plot origin
+  labs(x = "",                                   # plot to have no X- or Y- titles but a main plot title
+       y="", 
+       title = "Contributions of component indices to quarterly change in \nNICEI** (pps)"
+       ) 
+
+final_plot    #sanity check
+
+# save plot as image (text sizes need amended)
+# Note this is a repeat of plot script above except for text sizes
+final_plot_sized <- ggplot(df2, 
+                     aes(fct_reorder(Sector, desc(order)),   # attributes a sequence of plotting Sector based on 'order' 
+                         `Quarterly contribution (pps)*`,
+                         fill = category)) + 
+  geom_col(width = 0.25) +          # adjusts the column widths so they are thinner
+  theme_classic() +           # blank background as per original image
+  geom_text(aes(label =         # adding data labels to the plot
+                  ifelse(`Quarterly contribution (pps)*` < 0, `Quarterly contribution (pps)*`, "")),   # if value is negative 
+            hjust = 1.1) +     # position label on left
+  geom_text(aes(label =
+                  ifelse( `Quarterly contribution (pps)*` > 0, `Quarterly contribution (pps)*`, "")),  # if value is negative 
+            hjust = -0.2) +    # position label on left
+  scale_fill_manual(values= c("#000066",         # colour first category 
+                              "blue",            # colour second category 
+                              "#cccc00")) +      # colour third category 
+  theme(aspect.ratio = 1/2,                                         # change aspect ratio of plot
+        plot.margin = unit(c(2,2,2,2), "cm"),                       # pad area around plot for image capture
+        panel.grid.major.x = element_line(color = "grey"),          # colour major gridlines grey
+        plot.title = element_text(size = 17,                        # centre the plot title, size = 17 and bolden
+                                  hjust = 0.5,                      
+                                  face="bold"),
+        axis.text = element_text(size=13, face =                    # make axis text bold and size 13
+                                   "bold"),
+        axis.line = element_line(colour = "grey"),                  # colour axis lines grey 
+        axis.ticks = element_blank(),                               # remove axis ticks
+        legend.position="none") +                                   # no legend
+  coord_flip()+                                  # flip plot to horizontal         
+  scale_y_continuous(limits=c(-0.5,1.5),         # alter y axis to start at -0.5 and end at 1.5
+                     expand = c(0, 0)) +         # y axis limits to start at plot origin
+  labs(x = "",                                   # plot to have no X- or Y- titles but a main plot title
+       y="", 
+       title = "Contributions of component indices to quarterly change in \nNICEI** (pps)"
+  ) 
+png("contributions_resized.png", width = 950, height = 750)
+final_plot_sized
+dev.off()
